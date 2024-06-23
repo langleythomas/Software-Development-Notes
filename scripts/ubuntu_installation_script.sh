4cff59db62f414ead84ca9155def353cf0f58b87
langleythomas <48969547+langleythomas@users.noreply.github.com>
2024-06-24 00:35:03 +0100

    Update ubuntu_installation_script.sh

--- scripts/ubuntu_installation_script.sh
+++ scripts/ubuntu_installation_script.sh
@@ -5,17 +5,21 @@
 #######################################################################################################################
 function update_and_upgrade_apt() {
   printf "Updating and updating releases in apt.\n"
+
   sudo apt update
+
   sudo apt upgrade --yes
 }
 
 function update_flatpak() {
   printf "Updating releases in Flatpak.\n"
+
   flatpak update
 }
 
 function update_snap() {
   printf "Updating releases in Snap.\n"
+
   sudo snap refresh
 }
 
@@ -32,13 +36,16 @@ function print_separator() {
 #######################################################################################################################
 
 function purge_nvidia_drivers() {
-  # Run this command if there are issues booting into an Ubuntu installation. This failure could be caused by Nvidia driver issues. The commands in this function can resolve this issue.
-  # Note: These commands can only be effective after a clean OS installation.
+  # Run this command if there are issues booting into an Ubuntu installation. This failure could be caused by Nvidia
+  # driver issues. The commands in this function can resolve this issue. These commands can only be effective in a
+  # fresh OS installation.
+
   update_and_upgrade_apt
 
   printf "Purging Nvidia drivers.\n"
 
   sudo apt purge nvidia* --yes
+
   sudo ubuntu-drivers autoinstall
 
   print_separator
@@ -58,8 +65,11 @@ function install_dot_net_sdk() {
   printf "Installing .NET SDK.\n"
 
   sudo dpkg --purge packages-microsoft-prod && sudo dpkg --install packages-microsoft-prod.deb
+
   sudo apt install --yes dotnet-sdk-7.0
+
   dotnet --list-sdks
+
   dotnet --info
 
   print_separator
@@ -71,7 +81,9 @@ function install_dot_net_runtime() {
   printf "Installing .NET Runtime.\n"
 
   sudo apt install --yes aspnetcore-runtime-7.0
+
   dotnet --list-runtimes
+
   dotnet --info
 
   print_separator
@@ -90,11 +102,18 @@ function install_javascript_development_tools() {
   update_and_upgrade_apt
 
   printf "Installing JavaScript Development Tools.\n"
+
   sudo apt install --yes ca-certificates curl gnupg
+
   sudo mkdir --parents /etc/apt/keyrings
-  curl --fail --silent --show-error --location "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key" | sudo gpg --dearmor --output "/etc/apt/keyrings/nodesource.gpg"
+
+  curl --fail --silent --show-error --location "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key" \
+  | sudo gpg --dearmor --output "/etc/apt/keyrings/nodesource.gpg"
+
   NODE_MAJOR=20
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" \
+  | sudo tee /etc/apt/sources.list.d/nodesource.list
 
   sudo apt install nodejs --yes
 }
@@ -114,8 +133,11 @@ function install_docker() {
 
   # Add Docker's official GPG key:
   sudo apt install ca-certificates curl
+
   sudo install --mode=0755 --directory="/etc/apt/keyrings"
+
   sudo curl --fail --silent --show-error --location "https://download.docker.com/linux/ubuntu/gpg" --output "/etc/apt/keyrings/docker.asc"
+
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   # Add the repository to Apt sources:
@@ -138,6 +160,7 @@ function start_docker() {
   printf "Starting Docker.\n"
 
   sudo service docker start
+
   systemctl status docker.service
 
   print_separator
@@ -154,8 +177,13 @@ function test_docker_installation() {
 function install_minikube() {
   printf "Installing Minikube.\n"
 
-  curl --location --output minikube "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+  curl \
+    --location \
+    --output minikube \
+    "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+
   chmod +x minikube
+
   sudo mv minikube /usr/local/bin/
 
   print_separator
@@ -174,8 +202,13 @@ function install_kubernetes() {
 
   local -r latest_kubernetes_release="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 
-  curl --location --output "https://storage.googleapis.com/kubernetes-release/release/${latest_kubernetes_release}/bin/linux/amd64/kubectl"
+  curl \
+    --location \
+    --output \
+    "https://storage.googleapis.com/kubernetes-release/release/${latest_kubernetes_release}/bin/linux/amd64/kubectl"
+
   chmod +x ./kubectl
+
   sudo mv ./kubectl /usr/local/bin/kubectl
 
   print_separator
@@ -185,6 +218,7 @@ function test_kubernetes_installation() {
   printf "Testing Kubernetes Installation.\n"
 
   kubectl version
+
   kubectl cluster-info
 
   print_separator
@@ -195,7 +229,9 @@ function install_helm() {
   printf "Installing Helm.\n"
 
   curl "https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get-helm-3" > "get_helm.sh"
+
   chmod 700 "get_helm.sh"
+
   ./"get_helm.sh"
 
   print_separator
@@ -205,8 +241,11 @@ function test_helm_installation() {
   printf "Testing Helm Installation.\n"
 
   helm repo add bitnami "https://charts.bitnami.com/bitnami"
+
   helm install odoo bitnami/odoo --set serviceType=NodePort
+
   kubectl get pods | grep "Running"
+
   helm delete odoo
 
   print_separator
@@ -235,7 +274,11 @@ function install_vim() {
   update_and_upgrade_apt
 
   printf "Installing vim.\n"
-  printf "Note: vim-gtk3 is being installed, as that supports copying and pasting to and from the system clipboard. vim-gnome is not being installed, as that is not in the repositories of the latest Ubuntu releases.\n"
+
+  printf "Note: vim-gtk3 is being installed, as that supports copying and pasting to and from the system clipboard\n."
+
+  printf "vim-gnome is not being installed, as that is not in the repositories of the latest Ubuntu releases.\n"
+
   sudo apt-get install vim-gtk3 --yes
 
   print_separator
@@ -243,30 +286,48 @@ function install_vim() {
 
 function configure_vim() {
   printf "Creating Vim configuration directories and .vimrc\n"
+
   mkdir --verbose --parents ~/".vim" ~/".vim/autoload" ~/".vim/backup" ~/".vim/colors" ~/".vim/plugged"
+
   touch ~/".vimrc"
 
-  curl "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/vim-configuration/.vimrc" >> ~/".vimrc"
-  # There is no need for a source command on the .vimrc, as the .vimrc is automatically read and validated when executing the vim command in a terminal.
+  curl \
+    "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/vim-configuration/.vimrc" \
+    >> ~/".vimrc"
+
+  # There is no need for a source command on the .vimrc, as the .vimrc is automatically read and validated when 
+  # executing the vim command in a terminal.
 }
 
 function install_visual_studio_code() {
   update_and_upgrade_apt
 
   printf "Installing Visual Studio Code.\n"
+
   sudo apt install wget gpg --yes
-  wget --quiet -output-document=- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+  wget --quiet -output-document=- https://packages.microsoft.com/keys/microsoft.asc \
+  | gpg --dearmor > packages.microsoft.gpg
+
   sudo install -D --owner=root --group=root --mode=644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" \
+  | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
   rm --force --verbose packages.microsoft.gpg
+
   update_and_upgrade_apt
+
   sudo apt install apt-transport-https --yes
+
   update_and_upgrade_apt
+
   sudo apt install code --yes
 
   print_separator
 
-  # If there is an issue loading the Visual Studio Code GUI after an update, as described here https://code.visualstudio.com/Docs/supporting/FAQ#_vs-code-is-blank:
+  # Execute the following command there is an issue loading the Visual Studio Code GUI after an update, as described
+  # here: https://code.visualstudio.com/Docs/supporting/FAQ#_vs-code-is-blank:
   # rm -r ~/.config/Code/GPUCache
 }
 
@@ -287,6 +348,7 @@ function install_git() {
   printf "Installing Git.\n"
 
   sudo apt install git --yes
+
   git --version
 
   print_separator
@@ -296,6 +358,7 @@ function configure_git_username_email() {
   printf "Setting up Git with a default username and email.\n"
 
   git config --global user.name "langleythomas"
+
   git config --global user.email "thomas.moorhead.langley@gmail.com"
 
   print_separator
@@ -305,10 +368,13 @@ function generate_github_ssh_key() {
   printf "Generating SSH key for cloning private GitHub repositories.\n"
 
   ssh-keygen -t ed25519 -C "thomas.moorhead.langley@gmail.com"
+
   eval "$(ssh-agent -s)"
+
   ssh-add ~/.ssh/id_ed25519
 
   printf "Open the generated ssh key.\n"
+
   cat ~/.ssh/id_ed25519.pub
 }
 
@@ -367,7 +433,10 @@ function install_vlc() {
 function configure_bashrc() {
   printf "Configuring .bashrc.\n"
 
-  curl "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/bash-configuration/.bashrc" >> ~/."bashrc"
+  curl \
+    "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/bash-configuration/.bashrc" \
+    >> ~/."bashrc"
+
   # shellcheck disable=SC1090
   source ~/."bashrc"
 
