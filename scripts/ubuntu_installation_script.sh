003de12a6534acf5ba5705179fd4b0bb344964da
langleythomas <48969547+langleythomas@users.noreply.github.com>
2024-07-07 14:50:19 +0100

    Update ubuntu_installation_script.sh

--- scripts/ubuntu_installation_script.sh
+++ scripts/ubuntu_installation_script.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env bash
 
 #######################################################################################################################
+################################################ Script Output Function ###############################################
+#######################################################################################################################
+
+function log_output() {
+  local -r logger_output="${1}"
+
+  printf "%s" "${logger_output}"
+  printf -- "-----------------------------------------------------------------------------------------------------------------------\n"
+}
+
+#######################################################################################################################
 ############################################## Generic Update Functions ###############################################
 #######################################################################################################################
 function update_and_upgrade_apt() {
-  printf "Updating and updating releases in apt.\n"
+  log_output "Updating and updating releases in apt.\n"
 
   sudo apt update
-
   sudo apt upgrade --yes
 }
 
 function update_flatpak() {
-  printf "Updating releases in Flatpak.\n"
+  log_output "Updating releases in Flatpak.\n"
 
   flatpak update
 }
 
 function update_snap() {
-  printf "Updating releases in Snap.\n"
+  log_output "Updating releases in Snap.\n"
 
   sudo snap refresh
-}
-
-#######################################################################################################################
-################################################ Script Output Functions ##############################################
-#######################################################################################################################
-
-function print_separator() {
-  printf -- "-----------------------------------------------------------------------------------------------------------------------\n"
 }
 
 #######################################################################################################################
@@ -42,13 +44,10 @@ function purge_nvidia_drivers() {
 
   update_and_upgrade_apt
 
-  printf "Purging Nvidia drivers.\n"
+  log_output "Purging Nvidia drivers.\n"
 
   sudo apt purge nvidia* --yes
-
   sudo ubuntu-drivers autoinstall
-
-  print_separator
 }
 
 function call_nvidia_driver_function() {
@@ -62,31 +61,24 @@ function call_nvidia_driver_function() {
 function install_dot_net_sdk() {
   update_and_upgrade_apt
 
-  printf "Installing .NET SDK.\n"
+  log_output "Installing .NET SDK.\n"
 
   sudo dpkg --purge packages-microsoft-prod && sudo dpkg --install packages-microsoft-prod.deb
-
   sudo apt install --yes dotnet-sdk-7.0
 
   dotnet --list-sdks
-
   dotnet --info
-
-  print_separator
 }
 
 function install_dot_net_runtime() {
   update_and_upgrade_apt
 
-  printf "Installing .NET Runtime.\n"
+  log_output "Installing .NET Runtime.\n"
 
   sudo apt install --yes aspnetcore-runtime-7.0
 
   dotnet --list-runtimes
-
   dotnet --info
-
-  print_separator
 }
 
 function call_dot_net_development_tool_functions() {
@@ -101,10 +93,9 @@ function call_dot_net_development_tool_functions() {
 function install_javascript_development_tools() {
   update_and_upgrade_apt
 
-  printf "Installing JavaScript Development Tools.\n"
+  log_output "Installing JavaScript Development Tools.\n"
 
   sudo apt install --yes ca-certificates curl gnupg
-
   sudo mkdir --parents /etc/apt/keyrings
 
   curl \
@@ -114,9 +105,9 @@ function install_javascript_development_tools() {
     --location "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key" \
     | sudo gpg --dearmor --output "/etc/apt/keyrings/nodesource.gpg"
 
-  NODE_MAJOR=20
+  local -r node_version=20
 
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" \
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_version.x nodistro main" \
     | sudo tee /etc/apt/sources.list.d/nodesource.list
 
   sudo apt install nodejs --yes
@@ -133,7 +124,7 @@ function call_javascript_development_tool_functions() {
 function install_docker() {
   update_and_upgrade_apt
 
-  printf "Installing Docker.\n"
+  log_output "Installing Docker.\n"
 
   # Add Docker's official GPG key:
   sudo apt install ca-certificates curl
@@ -161,53 +152,43 @@ function install_docker() {
 
   # Add user to Docker group
   sudo usermod --append --groups docker "${USER}" && newgrp docker
-
-  print_separator
 }
 
 function start_docker() {
-  printf "Starting Docker.\n"
+  log_output "Starting Docker.\n"
 
   sudo service docker start
 
   systemctl status docker.service
-
-  print_separator
 }
 
 function test_docker_installation() {
-  printf "Running a Test Docker Image.\n"
+  log_output "Running a Test Docker Image.\n"
 
   sudo docker run hello-world
-
-  print_separator
 }
 
 function install_minikube() {
-  printf "Installing Minikube.\n"
+  log_output "Installing Minikube.\n"
 
   curl \
     --location \
-    --output minikube \
+    --output "minikube" \
     "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
 
-  chmod +x minikube
+  chmod +x "minikube"
 
-  sudo mv minikube /usr/local/bin/
-
-  print_separator
+  sudo mv "minikube" "/usr/local/bin/"
 }
 
 function start_minikube() {
-  printf "Starting Minikube.\n"
+  log_output "Starting Minikube.\n"
 
   minikube start
-
-  print_separator
 }
 
 function install_kubernetes() {
-  printf "Installing Kubernetes.\n"
+  log_output "Installing Kubernetes.\n"
 
   local -r latest_kubernetes_release="$(curl --silent https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 
@@ -217,51 +198,34 @@ function install_kubernetes() {
     "https://storage.googleapis.com/kubernetes-release/release/${latest_kubernetes_release}/bin/linux/amd64/kubectl"
 
   chmod +x ./kubectl
-
   sudo mv ./kubectl /usr/local/bin/kubectl
-
-  print_separator
 }
 
 function test_kubernetes_installation() {
-  printf "Testing Kubernetes Installation.\n"
+  log_output "Testing Kubernetes Installation.\n"
 
   kubectl version
-
   kubectl cluster-info
-
-  print_separator
 }
 
 # Helm
 function install_helm() {
-  printf "Installing Helm.\n"
+  log_output "Installing Helm.\n"
 
   local -r install_helm_file_name="get_helm.sh"
-
   curl "https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get-helm-3" > "${install_helm_file_name}"
-
   chmod 700 "${install_helm_file_name}"
-
   ./"${install_helm_file_name}"
-
   rm --force --verbose "${install_helm_file_name}"
-
-  print_separator
 }
 
 function test_helm_installation() {
-  printf "Testing Helm Installation.\n"
+  log_output "Testing Helm Installation.\n"
 
   helm repo add bitnami "https://charts.bitnami.com/bitnami"
-
   helm install odoo bitnami/odoo --set serviceType=NodePort
-
   kubectl get pods | grep "Running"
-
   helm delete odoo
-
-  print_separator
 }
 
 function call_deployment_tool_functions() {
@@ -286,58 +250,57 @@ function call_deployment_tool_functions() {
 function install_vim() {
   update_and_upgrade_apt
 
-  printf "Installing vim.\n"
-
-  printf "Note: vim-gtk3 is being installed, as that supports copying and pasting to and from the system clipboard\n."
-
-  printf "vim-gnome is not being installed, as that is not in the repositories of the latest Ubuntu releases.\n"
-
+  log_output "Installing vim.\n"
+  log_output "\tNote: vim-gtk3 is being installed, as that supports copying and pasting to and from the system clipboard\n."
+  log_output "\tvim-gnome is not being installed, as that is not in the repositories of the latest Ubuntu releases.\n"
   sudo apt-get install vim-gtk3 --yes
+}
 
-  print_separator
+function install_vundle() {
+  log_output "Installing Vundle, the Vim package manager, as documented in: https://github.com/iamcco/markdown-preview.nvim?tab=readme-ov-file#installation--usage\n"
+
+  git clone "https://github.com/VundleVim/Vundle.vim.git" ~/".vim/bundle/Vundle.vim"
 }
 
 function configure_vim() {
-  printf "Creating Vim configuration directories and .vimrc\n"
+  log_output "Creating Vim configuration directories and configuration file.\n"
 
   mkdir --verbose --parents ~/".vim" ~/".vim/autoload" ~/".vim/backup" ~/".vim/colors" ~/".vim/plugged"
-
   touch ~/".vimrc"
-
   curl \
-    "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/vim-configuration/.vimrc" \
+    "https://raw.githubusercontent.com/langleythomas/software-development-notes/main/vim-configuration/.vimrc" \
     >> ~/".vimrc"
 
-  # There is no need for a source command on the .vimrc, as the .vimrc is automatically read and validated when 
+  # There is no need for a source command on the .vimrc, as the .vimrc is automatically read and validated when
   # executing the vim command in a terminal.
+}
+
+function install_vundle_plugins() {
+  log_output "Installing Vim plugins using Vundle, as documented in: https://github.com/iamcco/markdown-preview.nvim?tab=readme-ov-file#installation--usage\n"
+
+  vim ~/".vimrc" --cmd "source %" +qall
+  vim +PluginInstall +qall
+  vim ~/".vimrc" -c "call mkdp#util#install()" +qall
 }
 
 function install_visual_studio_code() {
   update_and_upgrade_apt
 
-  printf "Installing Visual Studio Code.\n"
+  log_output "Installing Visual Studio Code.\n"
 
   sudo apt install wget gpg --yes
-
   wget --quiet -output-document=- "https://packages.microsoft.com/keys/microsoft.asc" \
-    | gpg --dearmor > packages.microsoft.gpg
-
+    | gpg --dearmor > "packages.microsoft.gpg"
   sudo install -D --owner=root --group=root --mode=644 "packages.microsoft.gpg" "/etc/apt/keyrings/packages.microsoft.gpg"
-
   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" \
-  | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-
-  rm --force --verbose packages.microsoft.gpg
-
-  update_and_upgrade_apt
-
-  sudo apt install apt-transport-https --yes
+    | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+  rm --force --verbose "packages.microsoft.gpg"
 
   update_and_upgrade_apt
+  sudo apt install apt-transport-https --ye
 
+  update_and_upgrade_apt
   sudo apt install code --yes
-
-  print_separator
 
   # Execute the following command there is an issue loading the Visual Studio Code GUI after an update, as described
   # here: https://code.visualstudio.com/Docs/supporting/FAQ#_vs-code-is-blank:
@@ -346,7 +309,9 @@ function install_visual_studio_code() {
 
 function call_text_editor_installation_functions() {
   install_vim
+  install_vundle
   configure_vim
+  install_vundle_plugins
 
   install_visual_studio_code
 }
@@ -358,35 +323,27 @@ function call_text_editor_installation_functions() {
 function install_git() {
   update_and_upgrade_apt
 
-  printf "Installing Git.\n"
+  log_output "Installing Git.\n"
 
   sudo apt install git --yes
-
   git --version
-
-  print_separator
 }
 
 function configure_git_username_email() {
-  printf "Setting up Git with a default username and email.\n"
+  log_output "Setting up Git with a default username and email.\n"
 
   git config --global user.name "langleythomas"
-
   git config --global user.email "thomas.moorhead.langley@gmail.com"
-
-  print_separator
 }
 
 function generate_github_ssh_key() {
-  printf "Generating SSH key for cloning private GitHub repositories.\n"
+  log_output "Generating SSH key for cloning private GitHub repositories.\n"
 
   ssh-keygen -t ed25519 -C "thomas.moorhead.langley@gmail.com"
-
   eval "$(ssh-agent -s)"
-
   ssh-add ~/.ssh/id_ed25519
 
-  printf "Open the generated ssh key.\n"
+  log_output "Opening the generated ssh key.\n"
 
   cat ~/.ssh/id_ed25519.pub
 }
@@ -395,8 +352,26 @@ function call_version_control_functions() {
   install_git
 
   configure_git_username_email
-
   generate_github_ssh_key
+}
+
+#######################################################################################################################
+####################################### Configure Linux System Overrides ##############################################
+#######################################################################################################################
+
+function configure_bashrc() {
+  log_output "Configuring .bashrc.\n"
+
+  curl \
+    "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/bash-configuration/.bashrc" \
+    >> ~/."bashrc"
+
+  # shellcheck disable=SC1090
+  source ~/".bashrc"
+}
+
+function call_linux_system_overrides_function() {
+  configure_bashrc
 }
 
 #######################################################################################################################
@@ -406,54 +381,33 @@ function call_version_control_functions() {
 function install_chrome() {
   update_flatpak
 
-  printf "Installing Chrome.\n"
+  log_output "Installing Chrome.\n"
 
   flatpak install flathub com.google.Chrome --yes
-
-  print_separator
 }
 
 function install_discord() {
   update_flatpak
 
-  printf "Installing Discord.\n"
+  log_output "Installing Discord.\n"
 
   flatpak install flathub com.discordapp.Discord --yes
-
-  print_separator
 }
 
 function install_gnome_tweaks() {
   update_and_upgrade_apt
 
-  printf "Installing GNOME Tweaks.\n"
+  log_output "Installing GNOME Tweaks.\n"
 
   sudo apt install gnome-tweaks
-
-  print_separator
 }
 
 function install_vlc() {
   update_and_upgrade_apt
 
-  printf "Installing VLC.\n"
+  log_output "Installing VLC.\n"
 
   flatpak install flathub org.videolan.VLC --yes
-
-  print_separator
-}
-
-function configure_bashrc() {
-  printf "Configuring .bashrc.\n"
-
-  curl \
-    "https://raw.githubusercontent.com/langleythomas/Software-Development-Notes/main/bash-configuration/.bashrc" \
-    >> ~/."bashrc"
-
-  # shellcheck disable=SC1090
-  source ~/."bashrc"
-
-  print_separator
 }
 
 function call_miscellaneous_tool_functions() {
@@ -464,8 +418,6 @@ function call_miscellaneous_tool_functions() {
   install_gnome_tweaks
 
   install_vlc
-
-  configure_bashrc
 }
 
 #######################################################################################################################
@@ -475,11 +427,9 @@ function call_miscellaneous_tool_functions() {
 function autoremove_unused_dependencies() {
   update_and_upgrade_apt
 
-  printf "Autoremoving Unused Dependencies.\n"
+  log_output "Autoremoving Unused Dependencies.\n"
 
   sudo apt autoremove --yes
-
-  print_separator
 }
 
 function call_autoremove_unused_dependency_function() {
